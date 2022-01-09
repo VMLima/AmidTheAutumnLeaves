d9e678c72b66dd7f631f5f523ecd48e2e08037af
HainekoHanefuwa <53151270+HainekoHanefuwa@users.noreply.github.com>
2022-01-09 02:21:11 -0800

    Time Tracking & Core Weather
    Time tracking, seasonal/diurnal temp things work. In theory so do other weathery things, but I'll have to verify.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,6 +21,21 @@ public class Player : MonoBehaviour
     public PlayerAttributeSO hunger;
     public PlayerAttributeSO thirst;
 
+
+    private float wetness; // How wet the player is from 0 (bone dry) to 100 (drenched from head to toe)
+    private float bodyTemp = 0; // How comfortable the player is from -20 (dying of hypothermia) to 20 (dying of heatstroke)
+    public int workDiff = 1; // Will be in the equation but always set to 1 for now.
+
+    // May end up not being used for now.
+    public bool inShelter = false; // FALSE if you're wandering around, TRUE if you're in some kind of shelter.
+    public bool campfire = false; // Whether or not you have a campfire going.
+    private float shelterCover = 0; // Current overhead cover from 0 to 1.
+    private float shelterWindBlock = 0; // Current protection from the wind from 0 to 1.
+
+    // Methods which allow you to change these values, but ONLY using get/set so that all attempts to increment these
+    // must pass through these methods, allowing us to ensure that everything that modifies them triggers everything it
+    // should and the values are clamped appropriately, etc.
+
     //thirst
     //weather -> warmth -> thirst/hunger rate change.
     //    weather-> Effects.  Since it ticks every duration.
@@ -31,6 +46,7 @@ public class Player : MonoBehaviour
     //     Tim got dis.
 
     public static Player instance;
+
     private void Awake()
     {
         instance = this;
@@ -55,6 +71,9 @@ public class Player : MonoBehaviour
     //      I dunno, I'll work it out later.
     public void everySecond()
     {
+        TimeManager.instance.incrementMinute();
+        WeatherManager.instance.PerSecondWeather();
+
         regen(ref healthCurrent, healthRate, healthMax);
         regen(ref staminaCurrent, staminaRate, staminaMax);
         regen(ref warmthCurrent, warmthRate, warmthMax);
@@ -70,6 +89,8 @@ public class Player : MonoBehaviour
         cleanUpValue(ref healthCurrent);
         cleanUpValue(ref staminaCurrent);
         cleanUpValue(ref warmthCurrent);
+
+
     }
 
     void cleanUpValue(ref float value)
@@ -122,5 +143,51 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public bool InShelter
+    {
+        get { return inShelter; }
+        set { inShelter = value; }
+    }
+    public float Wetness
+    {
+        get { return wetness; }
+        set
+        {
+            if (value > 100) { wetness = 100; }
+            if (value < 0) { wetness = 0; }
+            else { wetness = value; }
+        }
+    }
+
+    public float BodyTemp
+    {
+        get { return bodyTemp; }
+        set
+        {
+            bodyTemp = value;
+        }
+    }
+    public float ShelterCover
+    {   // Don't panic, currently unused.
+        get { return shelterCover; }
+        set
+        {
+            if (value < 0) { shelterCover = 0; }
+            else if (value > 0) { shelterCover = 1; }
+            else { shelterCover = value; }
+        }
+    }
+    public float ShelterWind
+    {
+        // Don't panic, currently unused.
+        get { return shelterWindBlock; }
+        set
+        {
+            if (value < 0) { shelterWindBlock = 0; }
+            else if (value > 0) { shelterWindBlock = 1; }
+            else { shelterCover = value; }
+        }
     }
 }
--- Assets/Scripts/WeatherManager.cs
+++ Assets/Scripts/WeatherManager.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class WeatherManager : MonoBehaviour
 {
-    //ASH WEATHER VARIABLES.
-    //
+    public Weather weather = new Weather();
+    // 
+    private float diurnalTemp = 15f; // 15 for european forest
+    private float Evaporation; // Wetness loss per second
+    private float sweatRate; // Wetness gained per second
+    
+    public float deltaTemp = 0; // Change in player body temperature
+    public float deltaWet = 0; // change in wetness per second
+    public float deltaThirst = 0; // Change in thirst from weather
+    public float deltaFood = 0; // Change in food from weather
+
+    // Used in temperature change calculations method.
+    private float tempFactor =0;
+    private float windFactor=1;
+    private float watrFactor=1;
+    private float windChill = 0;
+
+
+    // TEMPORARY
+    private float skillEnduranceMod = 1; // Modifier from Endurance.  Should be (1+End^1.33)/166 
+
     List<EffectScript> summerEffects;
     GameObject summerObject;
     List<EffectScript> springEffects;
@@ -34,10 +53,217 @@ public class WeatherManager : MonoBehaviour
         }
     }
 
+    public void PerSecondWeather() // Applies weather effects to the player.
+    {
+        PlayerAttributeSO Thirst = IncManager.instance.Get<PlayerAttributeSO>("Thirst");
+//        PlayerAttributeSO Hunger = IncManager.instance.Get<PlayerAttributeSO>("Hunger");
+
+        Sunlight(); // figures out how much sunlight affects the temperature
+        TimeofDay(); // updates the time of day string
+        Temperature(); // find the exterior weather temperature, must be before Wetness.
+
+        Player.instance.Wetness += Wetness(); // Sweat, evaporation, and rain. Must be before ChangeInTemp.
+        Player.instance.BodyTemp += ChangeInTemperature(); // Temp change based on weather.
+
+        // Subtract sweat from player's current thirst.
+        Thirst.addAmount(-1 * sweatRate);
+
+        Debug.Log("Sun:" + weather.Sun + " Temp:" + weather.CurrTemp + " deltaTemp:" + deltaTemp + " player wetness:" + Player.instance.Wetness);
+
+    }
+
+    public float ChangeInTemperature() // Figures out how fast the player's temperature changes.
+    {
+        PlayerAttributeSO Thirst = IncManager.instance.Get<PlayerAttributeSO>("Thirst");
+        //        PlayerAttributeSO Hunger = IncManager.instance.Get<PlayerAttributeSO>("Hunger");
+        // Set deltaTemp to a fresh 0.
+        deltaTemp = 0;
+
+        if (weather.CurrTemp <= 68)
+        {
+            // Calculates approximate wind chill.
+            windFactor = Mathf.Abs(weather.CurrTemp - 40) / 20;
+            windChill = windFactor * (weather.Wind / 1.2f);
+
+            // Calculates the effects of player's current wetness that magnify temp loss.
+            watrFactor = 1 + Player.instance.Wetness / 150 + weather.Humidity / 1.2f;
+
+            // Calculates the base temperature loss that gets divided by insulation.
+            tempFactor = 0.1f + (Mathf.Abs(weather.CurrTemp - 68 - windChill) / 70);
+
+            // Returns a value for how much the player's temperature changes per second.            
+            deltaTemp -= (tempFactor * watrFactor) / (11.5f + (GearManager.instance.gear.ColdInsulation / (1 + Player.instance.Wetness)));
+        }
+        else if (weather.CurrTemp >= 80)
+        {
+            // Simple version, if you can sweat enough you're fine, otherwise bad things, but quite not as bad with protection.
+            if(Thirst.amount > 0) { deltaTemp = 0; }
+            else
+            {
+                // Wind mildly increases heat transfer
+                windFactor = 1 + weather.Wind / 20;
+
+                // Produces gradually increasing base value that is divided later
+                tempFactor = Mathf.Pow((weather.CurrTemp - 80), 0.33f);
+
+                // 
+                deltaTemp += tempFactor * windFactor * (1+weather.Humidity) * weather.Sun / GearManager.instance.gear.HeatInsulation;
+            }
+        }
+        else
+        {
+            deltaTemp = 0;
+        }
+
+        return deltaTemp;
+    }
+
+    void Temperature()
+    {
+        // Does all of the math to figure out the current base temperature outside.
+
+
+        // Calculates base temperature for the outside world. Cloud albedo ranges from 0 to 0.9
+        // In general cloud values should be aimed at ~0 to 0.2 for nice weather, higher for especially bad weather.
+        // Could in theory use negative cloud values to sim high clouds that reflect heat back at the earth but
+        // let's not get too crazy.
+        weather.BaseTemp = weather.WethTemp + (int)(weather.SeasonTemp + diurnalTemp * weather.Sun * (1 - (weather.Clouds * 0.9)));
+
+        // The version that gets messed with.
+        weather.CurrTemp = weather.BaseTemp;
+
+        if (Player.instance.InShelter == true)
+        {
+            // Placeholder, if in shelter it sets current temp to 70 no matter what conditions are outside.
+            weather.CurrTemp = 70;
+        }
+    }
+
+    public float Wetness()
+    {
+        // Freshly sets per second change in wetness to 0.
+        WeatherManager.instance.deltaWet = 0;
+
+        // Effects of rainfall
+        if (weather.Snow == false && Player.instance.InShelter == false) // If you're not under cover and it's raining
+        {
+            deltaWet += weather.Rain * GearManager.instance.gear.WaterRes;
+        }
+        else if (weather.Snow == false && Player.instance.InShelter == true) // if you're under cover and it's raining
+        {
+            deltaWet += (weather.Rain * Player.instance.ShelterCover * GearManager.instance.gear.WaterRes);
+        }
+        else { } // if it is snowing, you don't get rained on.
+
+
+        // Calculates how much wetness loss per second based on Temp, humidty, and wind. Wind is the dominant lever.
+        // If you're curious, the math works out such that drying from 'drenched' to 'dry' takes ~3 hours in 70 F, 
+        // mildly breezy weather at 50% (0.5) relative humidity.
+        WeatherManager.instance.Evaporation = 0.2777f * ((weather.CurrTemp + 100) / 100) * (1.25f - weather.Humidity) * (1 + weather.Wind / 7);
+
+        // If you have a campfire going, you dry off faster. 
+        if (Player.instance.campfire == true) { Evaporation += 1; }
+
+        Sweat();
+        Evaporate();
+
+        // Apply values to wetness change this second.
+        WeatherManager.instance.deltaWet += WeatherManager.instance.sweatRate;
+        WeatherManager.instance.deltaWet -= WeatherManager.instance.Evaporation;
+
+        return deltaWet;
+    }
+    public float Sweat()
+    {
+        PlayerAttributeSO Thirst = IncManager.instance.Get<PlayerAttributeSO>("Thirst");
+
+        // The amount of water sweated out per second based on the weather assuming you have water to sweat out.
+        sweatRate = 0.00034721666f * Player.instance.workDiff * (230 + weather.CurrTemp) * (1.4f - weather.Humidity) / skillEnduranceMod;
+
+        if (Thirst.amount <= 0) { sweatRate = 0; }
+        else if (Thirst.amount - sweatRate < 0 && Thirst.amount > 0) { sweatRate = Thirst.amount; }
+
+
+        return sweatRate;
+    }
+
+    public float Evaporate()
+    {
+
+        if (Player.instance.Wetness <= 0) { Evaporation = 0; }
+        else if(Player.instance.Wetness < Evaporation) { Evaporation = Player.instance.Wetness; }
+
+        return Evaporation;
+    }
+
+
+    public void Sunlight()
+    {
+        // Figures out how 'up' the sun is and returns a value between ~-1.7 and 1.7 that's used to modify diurnal variation.
+
+        // Returns -1 or 1 so seasonal variation affects sunrise and sunset
+        if(TimeManager.instance.times.Minutes <=720) { TimeManager.instance.times.AMorPM = 1; }
+        else if (TimeManager.instance.times.Minutes >= 721) { TimeManager.instance.times.AMorPM = -1; }
+
+        // Uses the above variables to calculate the 'upness' of the sun, which is used to factor
+        // the weather.diurnalTemp modifier and figure out what time of day it is.*
+        weather.Sun = (450 - Mathf.Abs(TimeManager.instance.times.Minutes - 780) + (TimeManager.instance.times.AMorPM * weather.TimeShift)) / (300 + weather.TimeShift);
+
+    }
+
+    public void TimeofDay()
+    {
+        // The if stack to determine the time of day based on how 'up' the sun is. 
+//        Debug.Log("Working" + TimeManager.instance.times.AMorPM);
+
+        if (weather.Sun <= 0)
+        {
+//            Debug.Log("I think the sun isn't out!");
+            // Differentiates between different times while sun is down.
+            if (TimeManager.instance.times.Minutes < 286) { TimeManager.instance.times.TimeOfDay = "Wee Hours"; return; }
+            else if(TimeManager.instance.times.Minutes >= 286 && TimeManager.instance.times.Minutes <= 600) { TimeManager.instance.times.TimeOfDay = "Predawn"; return; }
+            else if (TimeManager.instance.times.Minutes > 1275) { TimeManager.instance.times.TimeOfDay = "Late Night"; return; }
+            else if (TimeManager.instance.times.Minutes > 1200) { TimeManager.instance.times.TimeOfDay = "Night"; return; }
+        }
+        else if (weather.Sun > 0 && TimeManager.instance.times.AMorPM == 1)
+        {
+//            Debug.Log("I think the sun is out and it's morningish!");
+            // Uses timeSwap to tell whether it's morning or evening
+            if (weather.Sun > 0 && TimeManager.instance.times.Minutes > 719 && TimeManager.instance.times.Minutes < 780) { TimeManager.instance.times.TimeOfDay = "Midday"; return; }
+            else if (weather.Sun <= 0.02) { TimeManager.instance.times.TimeOfDay = "Sunrise"; return; }
+            else if (weather.Sun <= 0.17) { TimeManager.instance.times.TimeOfDay = "Dawn"; return; }
+            else if (weather.Sun <= 0.34) { TimeManager.instance.times.TimeOfDay = "Early Morning"; return; }
+            else if (weather.Sun <= 0.68) { TimeManager.instance.times.TimeOfDay = "Midmorning"; return; }
+            else if (weather.Sun <= 1) { TimeManager.instance.times.TimeOfDay = "Late Morning"; return; }
+ 
+        }
+        else if (weather.Sun > 0 && TimeManager.instance.times.AMorPM == -1)
+        {
+//           Debug.Log("I think the sun is out and it's eveningish!");
+            // Uses timeSwap to tell whether it's morning or evening
+            if (weather.Sun <= 0.02) { TimeManager.instance.times.TimeOfDay = "Sunset"; return; }
+            else if (weather.Sun <= 0.17) { TimeManager.instance.times.TimeOfDay = "Dusk"; return; }
+            else if (weather.Sun <= 0.34) { TimeManager.instance.times.TimeOfDay = "Evening"; return; }
+            else if (weather.Sun <= 0.68) { TimeManager.instance.times.TimeOfDay = "Late Afternoon"; return; }
+            else if (weather.Sun <= 1) { TimeManager.instance.times.TimeOfDay = "Afternoon"; return; }
+
+        }
+
+    }
+    public void PerDayWeather()
+    {
+
+        // Calculates seasonal time shift based on day of the year, the '0.5' can be adjusted 
+        //later to return different seasonal variations. In its current form it should cause 
+        // the day to be roughly 6 hours longer at the summer solstice and vice versa.
+        weather.TimeShift.Equals(0.5 * (Mathf.Abs(TimeManager.instance.times.Day - 180) - 90));
+
+    }
+
     void Awake()
     {
         instance = this;
-        
+        TimeManager.instance.incrementSeason();
         loadSeasonData();
     }
 
